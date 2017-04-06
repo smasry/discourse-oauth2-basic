3f93a13af7903b621d970d90b8df25783e4e0cac
Samer Masry <samer.masry@gmail.com>
2017-04-06 16:22:24 -0700

    fix for user id

--- plugin.rb
+++ plugin.rb
@@ -104,16 +104,16 @@ class OAuth2BasicAuthenticator < ::Auth::OAuth2Authenticator
     elsif SiteSetting.oauth2_email_verified?
       result.user = User.where(email: Email.downcase(result.email)).first
       if result.user && user_details[:email]
-        ::PluginStore.set("oauth2_basic", "oauth2_basic_user_#{user_md5_hash}", {user_id: user_md5_hash})
+        ::PluginStore.set("oauth2_basic", "oauth2_basic_user_#{user_md5_hash}", {user_id: user.id})
       end
     end
 
-    result.extra_data = { oauth2_basic_user_id: user_md5_hash }
+    result.extra_data = { oauth2_basic_user_email_hash: user_md5_hash }
     result
   end
 
   def after_create_account(user, auth)
-    ::PluginStore.set("oauth2_basic", "oauth2_basic_user_#{user_md5_hash}", {user_id: user.id })
+    ::PluginStore.set("oauth2_basic", "oauth2_basic_user_#{auth[:extra_data][:oauth2_basic_user_email_hash]}", {user_id: user.id })
   end
 
   def username_suggestion(name)
