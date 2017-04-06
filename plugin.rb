5b3f8742e44baee87d816056768d414c45b9df2d
Samer Masry <samer.masry@gmail.com>
2017-04-06 14:57:35 -0700

    fix for sitesetting

--- plugin.rb
+++ plugin.rb
@@ -14,7 +14,7 @@ class ::OmniAuth::Strategies::Oauth2Basic < ::OmniAuth::Strategies::OAuth2
       id: access_token['id']
     }
   end
-  option :token_params, { redirect_uri: SiteSettings.oauth2_redirect_uri }
+  option :token_params, { redirect_uri: SiteSetting.oauth2_redirect_uri }
 end
 
 class OAuth2BasicAuthenticator < ::Auth::OAuth2Authenticator
